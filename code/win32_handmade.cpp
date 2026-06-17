027b26e115aab6913f0cf942caff6827f89759d7
DavidGitCoder <github@solutionzmail.com>
2026-06-18 00:16:18 +0200

    add the current date

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,8 +1,8 @@
 /*==========================================================================
   $File: $
-  $Date: $
+  $Date: June, 18 2026
   $Revision: $
-  $Creator: Casey Muratori $
+  $Creator: Casey Muratori David M.
   $Notice: (C) Copyright 2026 by Molly Rocket, Inc. All Rights Reserved. $
 ===========================================================================*/
 #include <Windows.h>
