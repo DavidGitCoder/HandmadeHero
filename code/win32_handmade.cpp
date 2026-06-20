45ffee68c5b7f7b8229bccd657c014d58424b171
DavidGitCoder <github@solutionzmail.com>
2026-06-20 14:11:51 +0200

    Day 03: initial commit

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -7,6 +7,26 @@
 ===========================================================================*/
 #include <Windows.h>
 
+// static can have different meanings
+// internal: can define a function as being local to the file that it's in
+// local : only exists within its function scope but retains its value
+// global : globally accessible (automatically initialized to 0)
+#define internal static  
+#define local_persist static 
+#define global_variable static 
+// a static variable  is:
+//   Created only once
+//   Keeps its value between function calls
+//   Exists for the lifetime of the program
+// TODO: This is a global for now
+global_variable bool Running;
+
+// DIB: Device Independent Bitmap.
+// That's the name Windows uses to talk about things we can write into
+internal void ResizeDIBSection()
+{
+}
+
 LRESULT CALLBACK MainWindowCallback(HWND Window,
 				    UINT Message,
 				    WPARAM WParam,
@@ -21,11 +41,16 @@ LRESULT CALLBACK MainWindowCallback(HWND Window,
       }break; // Casey prefers the break outside
     case WM_DESTROY:
       {
-	OutputDebugStringA("WM_DESTROY\n");
+	// TODO: Handle this as an error - recreate window?
+	Running = false;
       }break;
     case WM_CLOSE:
       {
-	OutputDebugStringA("WM_CLOSE\n");
+	// exit the message loop, we can pass a parameter that GetMessage() will return
+	// but in our game use case it won't be necessary
+	// PostQuitMessage(0);	
+	// TODO: Handle this with a message to the user?
+	Running = false;
       }break;
     case WM_ACTIVATEAPP:
       {
@@ -47,26 +72,24 @@ LRESULT CALLBACK MainWindowCallback(HWND Window,
 	// static here means we want the variable to persist akin to a global variable but within this case's scope
 	// only use when debugging
 	// it initializes only once
-	static DWORD Operation = WHITENESS;
+	local_persist DWORD Operation = WHITENESS;
 	// The PatBlt function paints the specified rectangle using the brush 
 	// that is currently selected into the specified device context. 
 	// The brush color and the surface color or colors are combined by using the specified raster operation.
-	PatBlt(DeviceContext, X, Y, Height, Width, Operation); 
-	if(Operation == WHITENESS)
-	  {
-	    Operation = BLACKNESS;
-	  }
-	else
-	  {
-	    Operation = WHITENESS;
-	  }
+	PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+
+	// Toggle operation for next call
+	Operation = (Operation == WHITENESS) ? BLACKNESS : WHITENESS;
+
         EndPaint(Window, &Paint); 
+
         return 0L; 
       }break;
     default:
       {
-	// OutputDebugStringA("default\n");
-	Result=DefWindowProc(Window,Message,WParam,LParam);
+	// Calls the default window procedure to provide default processing 
+	// for any window messages that an application does not process
+	Result = DefWindowProcA(Window,Message,WParam,LParam);
       }break;
   }
   return(Result);
@@ -81,17 +104,19 @@ int CALLBACK WinMain(HINSTANCE Instance,
   // WNDCLASS is a struct (lookup tagWNDCLASS)
   WNDCLASS WindowClass = {}; // initialize to 0 every bytes
   
-  // check if CS_OWNDC|CS_HREDRAW|CS_VREDRAW still matter per Casey 
-  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW; 
-  WindowClass.lpfnWndProc = MainWindowCallback; // callback called when Windows needs to send a message to have it do something
+  // check if CS_OWNDC|CS_HREDRAW|CS_VREDRAW still matter per Casey
+  //  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW; <-- not needed
+
+  // callback called when Windows needs to send a message to have it do something
+  WindowClass.lpfnWndProc = MainWindowCallback;  
   WindowClass.hInstance = Instance;
   WindowClass.lpszClassName = "HandmadeHeroWidowClass";
   
   // returns an Atom but won't be needed 
-  if(RegisterClass(&WindowClass))
+  if(RegisterClassA(&WindowClass))
   {
     HWND WindowHandle =
-      CreateWindowEx(
+      CreateWindowExA(
 		     0,
 		     WindowClass.lpszClassName,
 		     "Handmade Hero",
@@ -107,14 +132,17 @@ int CALLBACK WinMain(HINSTANCE Instance,
     if(WindowHandle)
       {
 	// start a message loop
-	MSG Message;
-	for(;;)
+	Running = true;
+	while(Running)
 	  {
-	    BOOL MessageResult = GetMessage(&Message,0, 0,0); //loops until it returns false
+	    MSG Message;
+	    BOOL MessageResult = GetMessageA(&Message,0, 0,0); //loops until it returns false
 		if (MessageResult > 0)
 		  { 
-		    TranslateMessage(&Message); 
-		    DispatchMessage(&Message); 
+		    TranslateMessage(&Message);
+		    // calls our callback MainWindowCallback() but Windows can call our callback function
+		    // outside of this function
+		    DispatchMessageA(&Message); 
 		  }
 		else // in case the handler is invalid
 		  {
